e2122bab703284faf93effc69bbd744b8a4d0bee
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-09 23:57:03 +0000

    code dice results and add error message

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ source "https://rubygems.org"
 
 gem "puma"
 gem "sinatra"
-
-
 gem "rackup", "~> 2.1"
-
 gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
--- dice.rb
+++ dice.rb
@@ -3,7 +3,12 @@
 require "sinatra"
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -15,3 +20,34 @@ require "sinatra/reloader"
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
+
+get("/dice/2/6") do
+  first_dice = rand(1..6)
+  second_dice = rand(1..6)
+  sum = first_dice + second_dice
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end 
+
+get("/dice/2/10") do
+  first_dice = rand(1..10)
+  second_dice = rand(1..10)
+
+  sum = first_dice + second_die
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
+end 
+
+
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
