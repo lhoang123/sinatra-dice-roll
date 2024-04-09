7d92be7887f1d1f9cf66b4e5f2875faa159aa135
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-09 23:30:51 +0000

    set up gemfile

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# /Gemfile
+source "https://rubygems.org"
+
+gem "puma"
+gem "sinatra"
+
+
+gem "rackup", "~> 2.1"
+
+gem "sinatra-contrib"
---
+++ dice.rb
@@ -0,0 +1,17 @@
+# /dice.rb
+
+require "sinatra"
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+require "sinatra/reloader"
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
