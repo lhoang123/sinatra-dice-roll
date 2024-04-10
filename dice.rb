734753151fd708695c4a023c90e473c2b11b4a95
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 02:57:21 +0000

    layout

--- dice.rb
+++ dice.rb
@@ -13,11 +13,12 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_dice = rand(1..6)
-  second_dice = rand(1..6)
-  sum = first_dice + second_dice
+  @rolls = []
+  2.times do
+    die = rand(1..6)
 
-  @outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+    @rolls.push(die)
+  end
 
   erb(:two_six)
 end 
@@ -40,12 +41,22 @@ second_dice = rand (1..4)
 third_dice = rand(1..4)
 sum = first_dice + second_dice + third_dice
 
-outcome = "You rolled a #{first_dice} and a #{second_dice} and a #{third_dice} for a total of #{sum}."
+@outcome = "You rolled a #{first_dice} and a #{second_dice} and a #{third_dice} for a total of #{sum}."
 
-"<h1>4d4</h1>
-<p>#{outcome}</p>"
+erb(:four_four)
 end 
 
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+end
 
 require "better_errors"
 require "binding_of_caller"
