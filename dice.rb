b5834bf8febb6ed57c76c3a7b24eeb78ad3e3e82
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 00:02:02 +0000

    add links

--- dice.rb
+++ dice.rb
@@ -7,6 +7,7 @@ get("/") do
   <ul>
     <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/4/4\">Roll four 4-sided dice</a></li>
   </ul>
   "
 end
@@ -41,6 +42,18 @@ get("/dice/2/10") do
 
   "<h1>2d10</h1>
   <p>#{outcome}</p>"
+end 
+
+get("/dice/4/4") do
+first_dice = rand(1..4)
+second_dice = rand (1..4)
+third_dice = rand(1..4)
+sum = first_dice + second_dice + third_dice
+
+outcome = "You rolled a #{first_dice} and a #{second_dice} and a #{third_dice} for a total of #{sum}."
+
+"<h1>4d4</h1>
+<p>#{outcome}</p>"
 end 
 
 
