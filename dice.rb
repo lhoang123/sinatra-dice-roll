fdc2de4c024830da2c94fe546a45bdec90437406
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 02:13:36 +0000

    embedding ruby for homepage

--- dice.rb
+++ dice.rb
@@ -2,16 +2,6 @@
 
 require "sinatra"
 
-get("/") do
-  "<h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/4/4\">Roll four 4-sided dice</a></li>
-  </ul>
-  "
-end
-
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
@@ -64,3 +54,9 @@ require "binding_of_caller"
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+# /dice.rb
+	
+get("/") do
+  erb(:elephant)
+end
